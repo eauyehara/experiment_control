f537dfae4457d5540f0de85a04379bfd1bc4c7e0
lexmstr <60751507+lexmstr@users.noreply.github.com>
2020-04-10 15:27:46 -0400

    Debugging and some improvements
    Communication with sourcemeter corrected
    Added save data and plot

--- 428hub/dark_counts_vs_voltage.py
+++ 428hub/dark_counts_vs_voltage.py
@@ -1,24 +1,26 @@
 import pyvisa
 import numpy as np
 import time
+import csv
 import matplotlib.pyplot as plt
 
 USB_adress_COUNTER = 'USB0::0x0957::0x1807::MY50009613::INSTR'
 USB_adress_SOURCEMETER = 'USB0::0x0957::0x8C18::MY51141236::INSTR'
 
-Vbd = 25.1 # [V]
+Vbd = 24.2 # [V]
 max_overbias = 10 # [%]
 step_overbias = 1 # [%] Each step 1% more overbias
 
 
 # Frequency measurements settings
 slope = 'NEG' # Positive('POS')/ Negative('NEG') slope trigger
-threshold = 0 # [V] (absolute)
+threshold = -0.003 # [V] (absolute)
 
 def open_SourceMeter():
     SOURCEMETER = rm.open_resource(USB_adress_SOURCEMETER)
     SOURCEMETER.write('*RST') # Reset to default settings
-    SOURCEMETER.write(':SENS:CURR:PROT 100E-06') # Set compliance at 100 uA
+    SOURCEMETER.write(':SOUR1:FUNC:MODE VOLT')
+    SOURCEMETER.write(':SENS1:CURR:PROT 100E-06') # Set compliance at 100 uA
 
     return SOURCEMETER
 
@@ -44,13 +46,14 @@ def open_FreqCounter():
 def take_measure(COUNTER, SOURCEMETER, Vbias):
     # Set voltage to Vbias
     SOURCEMETER.write(':SOUR1:VOLT {}'.format(Vbias))
-    SOURCEMETER.write(':INIT')
+    SOURCEMETER.write(':OUTP ON')
+    time.sleep(0.5)
 
     # Initiate couting
     COUNTER.write('INIT') # Initiate couting
     COUNTER.write('*WAI')
     num_counts = COUNTER.query_ascii_values('FETC?')
-    return num_counts
+    return num_counts[0]
 
 
 # Bring the SPAD from 0V to Vbias at Vbias V/step
@@ -60,7 +63,7 @@ def bring_to_breakdown(SOURCEMETER, Vbd):
 
     while (Vinit < Vbd):
         SOURCEMETER.write(':SOUR1:VOLT {}'.format(Vinit))
-        SOURCEMETER.write(':INIT')
+        SOURCEMETER.write(':OUTP ON')
         Vinit = Vinit + Vstep
         time.sleep(0.5)
 
@@ -68,7 +71,7 @@ def bring_to_breakdown(SOURCEMETER, Vbd):
 
 
 rm = pyvisa.ResourceManager()
-#COUNTER = open_FreqCounter()
+COUNTER = open_FreqCounter()
 SOURCEMETER = open_SourceMeter()
 bring_to_breakdown(SOURCEMETER, Vbd)
 
@@ -77,7 +80,7 @@ num_measures = int(max_overbias/step_overbias) + 1 # 0% and max_overbias% includ
 vec_overbias = np.linspace(0, max_overbias, num = num_measures)
 voltage_counts = [vec_overbias , np.empty(num_measures), np.empty(num_measures)]
 
-for i in range (0, num_measures)
+for i in range (0, num_measures):
     voltage_counts[1][i] = Vbd + Vbd*vec_overbias[i]/100 # New overbias
     voltage_counts[2][i] = take_measure(COUNTER, SOURCEMETER, voltage_counts[1][i]) # Collect counts
 
@@ -87,4 +90,11 @@ plt.title('Dark counts')
 plt.xlabel('Reverse Bias Voltage [V]')
 plt.ylabel('Dark counts [1/s]')
 plt.grid(True)
-plt.show()
+plt.savefig("dark_counts_vs_overbias_Vbd_{}_{}max_{}step.png".format(Vbd, max_overbias, step_overbias))
+
+with open("dark_counts_vs_overbias_Vbd_{}_{}max_{}step.csv".format(Vbd, max_overbias, step_overbias), "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(map(lambda x: [x], voltage_counts[2]))
+
+COUNTER.close()
+SOURCEMETER.close()
