414129b249b5259a9fd48dcbcd3a7722f3c4bb96
Jaehwan <jhwnkim@mit.edu>
2020-06-24 01:20:27 -0400

    Update v-vs-counts.py
    Added loading of ND filter calibration and tap power coefficient values

--- 428hub/spad-tests/v-vs-counts.py
+++ 428hub/spad-tests/v-vs-counts.py
@@ -29,9 +29,7 @@ import csv
 from pint import Quantity as Q_
 from utils import *
 import matplotlib.pyplot as plt
-
-USB_adress_COUNTER = 'USB0::0x0957::0x1807::MY50009613::INSTR'
-USB_adress_SOURCEMETER = 'USB0::0x0957::0x8C18::MY51141236::INSTR'
+import pickle
 
 if len(sys.argv)>1:
 	Die = sys.argv[1]
@@ -40,7 +38,7 @@ else:
 
 Vbd = Q_(5.0, 'V') # [V]
 max_overbias = 10 # [%]
-step_overbias = 0.5 # [%] Each step 1% more overbias
+step_overbias =5 # to test 0.5 # [%] Each step 1% more overbias
 integration_time = 5.0 # sec
 bias_settle_time = 3.0 # sec
 
@@ -51,8 +49,7 @@ delta_thres = 0.0025 # Resolution of threshold trigger is 2.5 mV
 # thresholds = [-0.025, -0.05, -0.075]
 thresholds = [-0.015] # V
 
-
-
+# Filenames
 timestamp_str = datetime.strftime(datetime.now(),'%Y%m%d_%H%M%S-')
 fname = 'TC2_W3-5_PD4A-30um'
 csvname = timestamp_str+ fname+  '.csv'
@@ -60,6 +57,52 @@ imgname = timestamp_str+ fname+  '.png'
 temperature = 25.0
 
 experiment_info = 'Dark-integration {} sec, slope {}, bias settle time {} sec'.format(integration_time, slope, bias_settle_time)
+
+
+# Tap power to Incident Power coefficient
+ power_measurement = np.genfromtxt('C:/Users/poegroup/Documents/GitHub/jhwnkim/20200622/ND filter calibration/Pi-NE10B.csv', delimiter=',', skip_header=1)
+ wavelength = np.round(power_measurement[0])
+ tap_to_incident = power_measurement[5]
+
+
+# ND filter calibration values -
+nd_cfg = ["NE10B"]
+try:
+	pickle_in = open("config/nd_cal.pickle", "rb")
+	nd_filters = pickle.load(pickle_in)
+except:
+	nd_cal_dir = 'C:/Users/poegroup/Documents/GitHub/jhwnkim/20200622/ND filter calibration/'
+
+	print('No ND calibration value pickle file, generating from csv data set in {}'.format(nd_cal_dir))
+
+	nd_filters = {
+		"NE10B": 0,
+		"NE20B": 0,
+		"NE30B": 0,
+		"NE40B": 0,
+		"NE50A-A": 0,
+	}
+	for (filter, value) in nd_filters.items():
+		Pi = np.genfromtxt(nd_cal_dir+'Pi-'+filter+'.csv', delimiter=',', skip_header=1)
+		Po = np.genfromtxt(nd_cal_dir+'Po-'+filter+'.csv', delimiter=',', skip_header=1)
+
+		nd_filters[filter] = Po[1]/Pi[1]
+
+	pickle_out = open("config/nd_cal.pickle", "wb")
+	pickle.dump(nd_filters, pickle_out)
+	pickle_out.close()
+else:
+	print('ND calibration values loaded from nd_cal.pickle file')
+
+# Global instrument variables
+COUNTER = None
+SOURCEMETER = None
+POWERMETER = None
+
+USB_address_COUNTER = 'USB0::0x0957::0x1807::MY50009613::INSTR'
+USB_address_SOURCEMETER = 'USB0::0x0957::0x8C18::MY51141236::INSTR'
+USB_address_POWERMETER = 'USB0::0x1313::0x8079::P1001952::INSTR'
+
 
 # Open Frequency Counter and set it to count measurement
 def open_FreqCounter():
@@ -102,6 +145,8 @@ def sweep_threshold(COUNTER, SOURCEMETER, Vbias):
 	counts = []
 	for Vthresh in thresholds:
 		print('Counting at Vth = {} V'.format(Vthresh))
+		# Measure Counts
+		# Measure power
 		counts.append(take_measure(COUNTER, SOURCEMETER, Vbias, Vthresh))
 
 	return counts
